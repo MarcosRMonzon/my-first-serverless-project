97440fc3611eee3a4bcae5a6eedfe01303f19ee8
Marcos Monzón <marcosmonzonlsa@gmail.com>
2022-04-04 09:59:23 -0300

    Customizing the Request with Serverless Functions

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,11 +1,14 @@
 const axios = require("axios");
-exports.handler = async function() {
-  const response = await axios
-    .get("https://pokeapi.co/api/v2/pokemon/ditto")
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: response.data.name,
-        }),
-      };
-}
+exports.handler = async function(event, context) {
+  const eventBody = JSON.parse(event.body);
+  console.log(eventBody);
+  const response = await axios.get(
+    `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`,
+  );
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      pokemon: response.data.pokemon_entries,
+    }),
+  };
+};
